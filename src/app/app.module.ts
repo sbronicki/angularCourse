dab8110cc39110917de5f68db7c316c1621380cd
sbronicki <sbronicki@gmail.com>
2020-11-20 12:17:22 -0500

    Add ingredients & recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,21 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		RecipesComponent,
+		RecipeDetailComponent,
+		RecipeListComponent,
+		RecipeItemComponent,
+		ShoppingListComponent,
+		ShoppingEditComponent
+	],
+	imports: [ BrowserModule, FormsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { from } from 'rxjs';
 
@@ -8,11 +8,23 @@ import { from } from 'rxjs';
 	styleUrls: [ './recipe-list.component.css' ]
 })
 export class RecipeListComponent implements OnInit {
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
-		new Recipe('Tecipe', 'Very Tastey', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+		new Recipe(
+			'Test Recipe',
+			'Very Tastey',
+			'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+		),
+		new Recipe('Tecipe', 'Very Tastey', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+		new Recipe('Blecipe', 'Very Tastey', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
 	];
 
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	onRecipeSelected(recipe: Recipe) {
+		this.recipeWasSelected.emit(recipe);
+	}
 }
