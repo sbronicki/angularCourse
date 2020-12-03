4335ae403d44c76be0c5851f49aefafebfa2bfbb
sbronicki <sbronicki@gmail.com>
2020-12-03 12:42:04 -0500

    Edit Recipe update + add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,9 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-// import { HttpModule } from '';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
@@ -31,7 +30,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 		RecipeStartComponent,
 		RecipeEditComponent
 	],
-	imports: [ BrowserModule, FormsModule, AppRoutingModule ],
+	imports: [ BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule ],
 	providers: [ ShoppingListService ],
 	bootstrap: [ AppComponent ]
 })
