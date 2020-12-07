343c74241989f0cc3da739e2ff13e39302fba28e
sbronicki <sbronicki@gmail.com>
2020-12-07 16:46:18 -0500

    sign up handling + errors

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+interface AuthResponseData {
+	kind: string;
+	idToken: string;
+	email: string;
+	refreshToken: string;
+	expiresIn: string;
+	localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+	constructor(private http: HttpClient) {}
+
+	signup(email: string, password: string) {
+		return this.http
+			.post<
+				AuthResponseData
+			>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCAPn2kUcqFhncueu71w7cE5QNtjFwSHNk', {
+				email: email,
+				password: password,
+				returnSecureToken: true
+			})
+			.pipe(
+				catchError((errorRes) => {
+					let errorMessage = 'An unknown error occured';
+					if (!errorRes.error || !errorRes.error.error) {
+						return throwError(errorMessage);
+					}
+					switch (errorRes.error.error.message) {
+						case 'EMAIL_EXISTS':
+							errorMessage = 'This email is already taken';
+					}
+					return throwError(errorMessage);
+				})
+			);
+	}
+}
